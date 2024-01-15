a260e2203408e6f7cbbdd6ad3924c0b34c0bb3c8
Jaredslocker21 <jaredslocker21@gmail.com>
2024-01-15 01:25:37 +0000

    added all the bases for now

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn brittassite.wsgi
---
+++ brittas_blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+# brittas_blog/views.py
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+#def jewelry(request):
+    # Your view logic here
+#    return render(request, 'jewelry.html')  # Replace with the actual template name
+
+    # brittas_blog/views.py
+#from django.shortcuts import render
+
+#def art(request):
+    # Your view logic here
+#    return render(request, 'art.html')  # Replace with the actual template name
+
+#def about(request):
+    # Your view logic here
+#    return render(request, 'about.html')  # Replace with the actual template name
